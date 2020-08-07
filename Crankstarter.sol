cd0cdad72ed58ec63e4ef0361634bb79d58912cb
Eugene M <gaddeena@gmail.com>
2020-08-07 10:52:59 +0300

    Manual start; more events; autoclose.

--- Crankstarter.sol
+++ Crankstarter.sol
@@ -3,18 +3,21 @@ pragma solidity 0.7.0;
 
 /**
  * Yet another attempt at crowdfunding.
+ * The campain must be started manually by calling Start().
  **/
 contract Crankstarter {
     address payable public Creator = msg.sender;
     address payable public Beneficiary;
     
-    uint256 public Goal;
-    uint256 public Raised;
-    uint256 public Deadline;
+    uint public Goal;
+    uint public Raised;
+    uint public Duration;
+    uint public Deadline;
     
-    mapping(address => uint256) Backers;
+    mapping(address => uint) Backers;
     
     enum State {
+        Created,
         Fundraising,
         Expired,
         Successful,
@@ -24,24 +27,31 @@ contract Crankstarter {
     // For nicer messages from at_state modifier.
     mapping(State => string) private StateNames;
     
-    State public CurrentState = State.Fundraising;
+    State public CurrentState = State.Created;
     
-    event Backed(address backer, uint256 amount);
-    event Reclaimed(address backer, uint256 amount);
-    event Funded(uint256 amount);
+    event Started(uint t, uint duration, uint deadline);
+    event Backed(address backer, uint amount);
+    event Reclaimed(address backer, uint amount);
+    event Funded(uint amount);
     event Successful();
     event Expired();
+    event Closed();
     
     // Allows functions to be called only at certain states.
     modifier at_state(State s) {
-        require(CurrentState == s, string(abi.encodePacked("Operation is possible only in the ", StateNames[s], " state.")));
+        require(CurrentState == s, string(abi.encodePacked(
+            "Operation is possible only in the ",
+            StateNames[s],
+            " state. Currently it is ",
+            StateNames[CurrentState],
+            ".")));
         _;
     }
     
     // Automatically advances the funding state on every call.
     modifier advance_state() {
-        if (block.timestamp >= Deadline) {
-            if (CurrentState == State.Fundraising) {
+        if (CurrentState == State.Fundraising) {
+            if (block.timestamp >= Deadline) {
                 if (Raised >= Goal) {
                     CurrentState = State.Successful;
                     emit Successful();
@@ -61,18 +71,19 @@ contract Crankstarter {
         _;
     }
     
-    constructor(address payable beneficiary, uint256 goal, uint256 duration) {
+    constructor(address payable beneficiary, uint goal, uint duration) {
         Beneficiary = beneficiary;
         
         if (Beneficiary == address(0)) {
             Beneficiary = Creator;
         }
         
-        Goal = goal;
+        Goal = goal * 1 ether;
         Raised = 0;
-        Deadline = block.timestamp + duration * 10 seconds;
+        Duration = duration;
         
         // For nicer messages from at_state modifier.
+        StateNames[State.Created] = "Created";
         StateNames[State.Fundraising] = "Fundraising";
         StateNames[State.Successful] = "Successful";
         StateNames[State.Expired] = "Expired";
@@ -86,12 +97,21 @@ contract Crankstarter {
     }
     
     // Withdrawal pattern it is.
-    function reclaim() at_state(State.Expired) public {
+    function reclaim() advance_state at_state(State.Expired) public {
+        // In order to prevent re-entrancy attacks (when atatcker recursively calls reclaim()) do CEI:
         // Check
         require(Backers[msg.sender] > 0, "You haven't backed the project.");
-        uint256 amount = Backers[msg.sender];
+        uint amount = Backers[msg.sender];
         // Effect
         Backers[msg.sender] = 0;
+        Raised -= amount;
+        
+        // Autoclosing the fundraiser.
+        if (Raised == 0) {
+            CurrentState = State.Closed;
+            emit Closed();
+        }
+        
         // Interaction
         msg.sender.transfer(amount);
         emit Reclaimed(msg.sender, amount);
@@ -99,10 +119,23 @@ contract Crankstarter {
     
     // The project author comes here for their funding.
     // Withdrawal pattern again.
-    function claimFunds() by(Beneficiary) at_state(State.Successful) public {
+    function claimFunds() by(Beneficiary) advance_state at_state(State.Successful) public {
         Beneficiary.transfer(Raised);
         emit Funded(Raised);
+        
+        // Closing the fundraiser.
         Raised = 0;
         CurrentState = State.Closed;
+        emit Closed();
+    }
+    
+    function Start() by(Creator) at_state(State.Created) public {
+        Deadline = block.timestamp + Duration * 10 seconds;
+        CurrentState = State.Fundraising;
+        emit Started(block.timestamp, Duration, Deadline);
+    }
+    
+    function TimeLeft() advance_state public returns (uint) {
+        return Deadline - block.timestamp;
     }
 }
